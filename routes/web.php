fbcc81209528d77855e8a6df15d94198a4ba6b43
Luke McCann <developer-lukemccann@outlook.com>
2020-12-08 13:29:49 +0000

    Add basic routing examples

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::view('/view-only', 'welcome');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{params}', function ($params) {
+    return view('welcome', ['params' => $params]);
+});
+
+
