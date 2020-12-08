0e55881a82eac9838a050a981f45de46094b8f8e
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-08 23:00:11 +0000

    Add example of multiple validation rules

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -24,8 +24,8 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'username' => 'required',
-            'password' => 'required'
+            'username' => 'required|max:10',
+            'password' => 'required|min:6'
         ];
     }
 }
