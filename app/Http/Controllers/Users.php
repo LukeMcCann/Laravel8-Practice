5e66fd0c22876cdcb9e70ab3abbaad6176fcbb94
Luke McCann <developer-lukemccann@outlook.com>
2020-12-08 15:31:07 +0000

    Add API example

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -8,6 +8,13 @@ final class Users extends Controller
 {
     public function index(string $name)
     {
-        echo "Hello $name, from " . get_class($this);
+        echo "Hello $name, from " . get_class($this) . '<br /><br />';
+
+        // API Example: Note you would usually return a  new JsonResponse for an API
+
+        return  ['Users' => [
+                    'name' => $name
+                ]
+            ];
     }
 }
