378ff95208f26467fe1d10913db0d3716116a810
Luke McCann <developer-lukemccann@outlook.com>
2020-12-08 13:40:52 +0000

    Add anchor tag routing example

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Laravel</h1>
+    <h1>Contact</h1>
     <ul>
         <li><a href="/about"> About </a><li> 
         <li><a href="/contact"> Contact </a></li>
